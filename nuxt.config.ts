458b16012f984567f58608b365d0dfaa508bc5f3
YuanOwO <yuan@yuanowo.xyz>
2025-11-14 11:22:32 +0800

    build: Enable Nitro websocket and update tsconfig settings
    在 nuxt.config.ts 中啟用了 Nitro 的實驗性 WebSocket 支援。更新了 tsconfig.json，設置了 baseUrl，包括了 .vue 檔案，並從 .nuxt/tsconfig.json 擴展，以改善 TypeScript 配置。

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -30,4 +30,9 @@ export default defineNuxtConfig({
     '@composable': path.resolve(__dirname, './app/composable'),
     '@types': path.resolve(__dirname, './app/types')
   },
+  nitro: {
+    experimental: {
+        websocket: true
+    }
+  }
 })
