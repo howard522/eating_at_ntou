3d97a8d48015ccd276bda5974fb513ba9777196e
brotato777 <123862938+brotato777@users.noreply.github.com>
2025-11-15 21:47:11 +0800

    feat: 更新廣告內容及按鈕文字

--- app/layouts/AdLayout.vue
+++ app/layouts/AdLayout.vue
@@ -4,10 +4,10 @@
     <v-card>
       <v-card-title class="text-h6 font-weight-bold text-center">廣告</v-card-title>
       <v-card-text class="text-center py-4">
-        應徵廣告欄位
+        馬尚彬教授開設的軟體工程課程，帶您深入了解軟體開發的最佳實踐與工具！
       </v-card-text>
       <v-card-actions class="justify-center">
-        <v-btn color="primary" @click="closeAd">關閉</v-btn>
+        <v-btn color="primary" @click="closeAd">了解了</v-btn>
       </v-card-actions>
     </v-card>
   </v-dialog>
