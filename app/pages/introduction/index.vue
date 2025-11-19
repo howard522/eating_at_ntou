cd7ed564b1a4f111fa68a76d093ece2ad40f4e0d
brotato777 <123862938+brotato777@users.noreply.github.com>
2025-11-19 22:07:13 +0800

    feat: 更新介紹頁面內容，增強使用者體驗並調整滾動動畫效果

--- app/pages/introduction/index.vue
+++ app/pages/introduction/index.vue
@@ -1,11 +1,40 @@
 <template>
   <div class="black-screen" @click="goBack">
     <div class="scrolling-text">
-      <p>感謝您使用本系統</p>
-      <p>這是一個簡單的介紹頁面</p>
-      <p>點擊畫面任何地方返回上一頁</p>
-      <p>開發者：J</p>
-      <p>結束</p>
+      <p style="font-size: 28px; margin-bottom: 16px;">感謝您使用本系統</p>
+      <p>我們致力於打造一個</p>
+      <p>專屬於海大學生的餐飲外送平台</p>
+      <p>讓顧客能快速下單，外送員能便捷接單</p>
+      <p>系統能維護訂單流程完整，確保穩定無錯誤</p>
+      <br /> 
+      <br /> 
+      <br /> 
+      <p style="font-size: 28px; margin-bottom: 16px;">— 製作團隊 Production Team —</p> 
+      <p>前端技術總監（Front-End Director）</p> 
+      <p>前端大大大姐頭 KarryLin 林津瑄</p> 
+<br /> 
+      <p>前端開發（Front-End Developer）</p> 
+      <p>前端小弟兼首席組員溝通師 JosephLiu 劉俊麟</p> 
+<br /> 
+      <p>後端技術總監（Back-End Director）</p>
+      <p>後端老大 aka 傲嬌阿修羅 94SungLa 宋辰星</p> 
+<br /> 
+      <p>後端工程師（Back-End Developer）</p> 
+      <p>後端小弟兼文件風格管理員 YuanOwO 黃俊源</p> 
+<br /> 
+      <p>後端工程師（Back-End Developer）</p> 
+      <p>後端老弟兼荷包蛋 Howard 郭浩</p> 
+      <br /> <br /> <br /> 
+      <p>這個系統的誕生，就像拍攝一部電影</p> 
+      <p>一路走來有劇本的修改、有鏡頭的重來、有突發的插曲</p> 
+      <p>也有靈感閃現時像燈光打亮的瞬間</p> 
+      <p>在過程中，我們爭執過、崩潰過、通宵過</p> 
+      <p>也一起跨過了一次又一次的「這一定做不完吧」的山谷</p> 
+      <p>但也正是因為有大家的存在，這段旅程才不是一個人的獨白</p> 
+      <p>而是一支真正的劇組、一群可信賴的夥伴</p> 
+      <p>每一位組員，都在幕後留下了屬於自己的光</p> 
+      <p>在片尾的最後，我想留下這句話——</p> 
+      <p>謝謝你們，願下一部續集，我們還能一起上映。</p>
     </div>
   </div>
 </template>
@@ -39,13 +68,17 @@ export default {
   color: white;
   font-size: 24px;
   text-align: center;
-  animation: scrollUp 10s linear infinite;
-  animation-delay: -4s;
+  animation: scrollUp 37s linear infinite;
+  animation-delay: 0s;
+}
+
+.scrolling-text p {
+  margin-bottom: 25px;
 }
 
 @keyframes scrollUp {
   from {
-    transform: translateY(100vh);
+    transform: translateY(240vh);
   }
   to {
     transform: translateY(-100vh);
